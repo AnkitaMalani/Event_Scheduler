434f3e39bc4feda77f97e65bfb8694c7a635913a
Sven Kaack <sven.kaack@gmx.de>
2025-05-26 17:14:43 +0200

    created signup component and updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,22 @@ import { BrowserRouter as Router, Routes, Route } from "react-router";
 import MainLayout from "./layout/MainLayout";
 import Home from "./pages/Home";
 import EventPage from "./pages/EventPage";
-
-
+import SignUp from "./pages/SignUp";
 
 function App() {
-  return (
-    <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<MainLayout />}>
-            <Route index element={<Home />} />
-            <Route path="event" element={<EventPage />} />
-           
-          </Route>
-        </Routes>
-      </Router>
-    </>
-  );
+    return (
+        <>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<MainLayout />}>
+                        <Route index element={<Home />} />
+                        <Route path="event" element={<EventPage />} />
+                        <Route path="signup" element={<SignUp />} />
+                    </Route>
+                </Routes>
+            </Router>
+        </>
+    );
 }
 
 export default App;
