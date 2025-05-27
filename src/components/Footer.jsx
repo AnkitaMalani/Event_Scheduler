8224002f6fd5dd672cb04e4ed77cc2a97d3a1226
Sven Kaack <sven.kaack@gmx.de>
2025-05-28 00:12:47 +0200

    created header/navbar and redirections, footer, updated me function in auth.js, updated MainLayout, SignIn and Signout components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <footer className="flex justify-center bg-zinc-700 py-4 text-2xl w-full text-white text-sm">
+            © 2025 Event Scheduler. All rights reserved. Manage and organize
+            your events efficiently and securely.
+        </footer>
+    );
+};
+
+export default Footer;
--- src/data/auth.js
+++ src/data/auth.js
@@ -39,7 +39,7 @@ const me = async () => {
 
     if (!token) throw new Error(`Failed to sign in. Please try again.`);
 
-    const res = await fetch(`${BASE_URL}/me`, {
+    const res = await fetch(`${BASE_URL}/auth/profile`, {
         headers: {
             Authorization: `Bearer ${token}`,
         },
